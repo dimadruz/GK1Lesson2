4d15e916ea220cd12f17fa942731e4577331bf11
dimadruz <78649700+dimadruz@users.noreply.github.com>
2021-02-17 21:46:51 +0300

    Update Lesson2.java

--- src/main/java/Lesson2.java
+++ src/main/java/Lesson2.java
@@ -23,10 +23,13 @@ public class Lesson2 {
     }
     //Задание 3
     public static void bar(){
-        int [] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        for ( int i = 0; i<arr.length;i++){
-            if(arr[i]<6){
-                System.out.print(arr[i] * 2 + " ");
+             int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr [i]< 6)  {
+                arr [i] *= 2;
+                System.out.println(arr[i] + "");
+            }
+        }
             }
         }
     }
